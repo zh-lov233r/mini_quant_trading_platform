d251c686528d013be888cff566339739489a782f
lov233r <ziyuehua@andrew.cmu.edu>
2025-08-16 17:58:27 -0700

    update backend structure and added some critical files to backend

---
+++ backends/src/services/backtest_engine.py
@@ -0,0 +1,13 @@
+# 执行回测/参数优化
+
+
+# 
+def run_backtest_trend():
+    pass
+
+
+# 
+def run_backtest_median():
+    pass
+
+
---
+++ backends/src/services/strategy_engine.py
@@ -0,0 +1,10 @@
+# 注册策略，生成买入/卖出信号
+
+
+def generate_signals():
+    pass
+
+
+
+
+
