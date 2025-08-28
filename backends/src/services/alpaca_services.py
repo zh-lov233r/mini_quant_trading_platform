7204d6306494ac9ce3ba9087c74b5f00c5f2ba46
lov233r <ziyuehua@andrew.cmu.edu>
2025-08-28 16:30:05 -0700

    add scripts for finding stock data

--- backends/src/services/alpaca_services.py
+++ backends/src/services/alpaca_services.py
@@ -1,11 +1,18 @@
 # 执行交易
 
 
-def send_order():
-    pass
+import requests, os
 
+ALPACA_KEY = os.getenv("ALPACA_KEY")
+ALPACA_SECRET = os.getenv("ALPACA_SECRET")
+BASE_URL = "https://paper-api.alpaca.markets/v2"
 
-
+def place_order(symbol: str, qty: int, side: str="buy", type: str="market", tif: str="day"):
+    url = f"{BASE_URL}/v2/orders"
+    headers = {"APCA-API-KEY-ID":ALPACA_KEY,"APCA-API-SECRET-KEY":ALPACA_SECRET}
+    order = {"symbol":symbol,"qty":qty,"side":side,"type":type,"time_in_force":tif}
+    r = requests.post(url,json=order,headers=headers)
+    return r.json()
 
 
 
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main style={{ maxWidth: 680, margin: "48px auto", fontFamily: "system-ui" }}>
       <h1 style={{ marginBottom: 12 }}>Quant Frontend</h1>
-      <p style={{ marginBottom: 16 }}>这是首页。去创建一个趋势策略吧：</p>
+      <p style={{ marginBottom: 16 }}>This is Main Page, Create a New Strategy!</p>
       <Link href="/strategies/new" style={{ color: "#2563eb" }}>
         /strategies/new
       </Link>
